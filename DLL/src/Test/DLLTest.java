28e0c82187033e0d317b27343062ce0a3d16a558
Alexander Widerin <Alexa@DESKTOP-503JLG9>
2018-11-20 14:38:58 +0100

    DLL Test

--- DLL/src/Test/DLLTest.java
+++ DLL/src/Test/DLLTest.java
@@ -1,33 +1,48 @@
 package Test;
 
-import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import DLL.LinkedList;
 
 public class DLLTest {
+	public static LinkedList<String> t1;
+	public static LinkedList<String> t2;
+	public static LinkedList<String> t3;
+	@BeforeClass
+	public void lists() {
+		t1= new LinkedList<>();
+		t2= new LinkedList<>();
+		t3= new LinkedList<>();
+	}
 	@Test
-	public void removeTest() {
-		LinkedList<String> test= new LinkedList<>();
-		test.add("Hallo");
-		test.add("du");
-		test.add("Bist");
-		test.remove(0);
-		assertEquals("du", test.getValue(0));
+	public void getValueTest() {
+		t1.add("Hallo");
+		t1.add("du");
+		t1.add("Bist");
+		assertEquals("du", t1.getValue(1));
 	}
 	
 	@Test
+	public void removeTest() {
+		
+		t2.add("Hallo");
+		t2.add("du");
+		t2.add("Bist");
+		t2.remove(0);
+		assertEquals("du", t2.getValue(0));
+	}
+	
+	
+	@Test
 	public void sizeTest() {
-		LinkedList<String> test= new LinkedList<>();
-		test.add("Hallo");;
-		test.add("du");;
-		test.add("bist");
-		assertEquals(3, test.getSize());
+		
+		t3.add("Hallo");;
+		t3.add("du");;
+		t3.add("bist");
+		t3.remove(2);
+		assertEquals(2, t3.getSize());
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
