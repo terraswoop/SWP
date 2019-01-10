9f50bda90cd70ea83cf5ba81acf3fdb4cd05dd86
Widerin Alexander <38126262+WildWolfAlex@users.noreply.github.com>
2019-01-10 08:44:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Sort
+{
+    class Program
+    {
+        public static int[] insertionSort(int[] A)
+        {                      
+            for (int i = 1; i < A.Length; i++)
+            {
+                int h = A[i];
+                int j = i - 1;
+                while(j >= 0 && A[j]>h )
+                {
+                    A[j + 1] = A[j];
+                    j = j - 1;
+                }
+                A[j + 1] = h;
+            }
+            return A;
+        }
+        static void Main(string[] args)
+        {
+            int[] A = { 4, 2, 3, 1 };
+            A = insertionSort(A);
+            for(int i = 0; i < A.Length; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
