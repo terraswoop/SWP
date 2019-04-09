9f45ce8da37c6836fdf70bb95314fa15afc00110
Alexander Widerin <Alexa@DESKTOP-503JLG9>
2019-04-09 14:36:16 +0200

    adminSearch

--- Kundenauftrag/auth3/users/admin.py
+++ Kundenauftrag/auth3/users/admin.py
@@ -14,20 +14,41 @@ class CustomUserAdmin(UserAdmin):
 
 class ClassAdmin(admin.ModelAdmin):
     list_display = ['class_name']
+    search_fields=('class_name',)
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'name']
+    search_fields=('name',)
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display= ['first_name', 'name']
+    search_fields=('name',)
+
+class StusuAdmin(admin.ModelAdmin):
+    search_fields=('student__name','subject__name')
+
+class TeasuAdmin(admin.ModelAdmin):
+    search_fields=('teacher__name','subject__name')
+
+class SubjectAdmin(admin.ModelAdmin):
+    search_fields=('name',)
+    
+class ExamAdmin(admin.ModelAdmin):
+    search_fields=('name',)
+
+class ClasuAdmin(admin.ModelAdmin):
+    search_fields=('klasse__class_name','subject__name')
+
+class ClateaAdmin(admin.ModelAdmin):
+    search_fields=('klasse__class_name','teacher__name')
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Class, ClassAdmin)
 admin.site.register(Teacher, TeacherAdmin)
 admin.site.register(Student, StudentAdmin)
-admin.site.register(Subject)
-admin.site.register(Exam)
-admin.site.register(Teasu)
-admin.site.register(Clasu)
-admin.site.register(Clatea)
-admin.site.register(Stusu)
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Exam, ExamAdmin)
+admin.site.register(Teasu, TeasuAdmin)
+admin.site.register(Clasu, ClasuAdmin)
+admin.site.register(Clatea, ClateaAdmin)
+admin.site.register(Stusu, StusuAdmin)
